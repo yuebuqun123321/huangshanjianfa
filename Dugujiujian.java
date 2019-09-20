ed40a2451cc73a93531aeb59ca52ff92babeee9f
yuebuqun123321 <3084958921@qq.com>
2019-09-20 14:47:07 +0800

    yuebuqun is laoda

--- Dugujiujian.java
+++ Dugujiujian.java
@@ -2,5 +2,6 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("du gu jiu jian zhen lihai ");
 		System.out.println("ni de jianfa henlaji ,kuaidian qu zigong ");
+		System.out.println("wocaishi lao da !!!");
 	}
 }
